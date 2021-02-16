14e8e9cdb1b32c753437475dee1e92bc4d2a6a97
Your Name <you@example.com>
2021-02-16 19:06:20 +0900

    make test file and delete unneed file

---
+++ test file/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* test */
+int test1(int a, int b){
+	// hihi
+  return a + b;
+}
+
+int test2(){
+  // git test
+  printf("test2");
+  // ds
+}
+
+int test3() {
+  printf("conflict")
+}
+
+int main()
+{
+  return 0;
+}
